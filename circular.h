3d0ffb65d3164660a9b9b316a7ef2d04371d8563
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-09-12 23:50:33 -0500

    Update circular.h

--- circular.h
+++ circular.h
@@ -2,58 +2,144 @@
 #define CIRCULAR_H
 
 #include "list.h"
+#include <iostream>
+#include <iterators/bidirectional_iterator.h>
+#include <algorithm>
+using namespace std;
 
 template <typename T>
 class CircularLinkedList : public List<T> {
     public:
         CircularLinkedList() : List<T>() {}
 
-        T front() {
-            // TODO
-        }
+    T front() {
+        if(this->head!= nullptr)
+            return this->head->data;
+        else throw invalid_argument("error");
+    }
 
-        T back() {
-            // TODO
-        }
+    T back() {
+        if(this->head!= nullptr)
+            return this->tail->data;
+        else throw invalid_argument("error");
+    }
 
         void push_front(T value) {
-            // TODO
+            auto current=this->head;
+            auto temp=new Node<T>;
+
+            temp->data=value;
+            while (current->next!=this->head)
+                current=current->next;
+            temp->next=temp;
+            if(this->head==nullptr){
+                this->head=temp;
+            }else {
+                temp->next=this->head;
+                current->next=temp;
+                this->head=temp;
+            }
         }
 
         void push_back(T value) {
-            // TODO
+            auto current=this->head;
+            auto temp=new Node<T>;
+            temp->data=value;
+            while (current->next!=this->head)
+                current=current->next;
+            temp->next=temp;
+            if(this->head==nullptr){
+                this->head=temp;
+            }else {
+                temp->next=this->head;
+                current->next=temp;
+            }
         }
 
         void pop_front() {
-            // TODO
-        }
+            if(this->nodes!=0){
+            auto current=this->head;
+            auto temp=new Node<T>;
+            while (current->next!=this->head){
+                temp=current;
+                current=current->next;}
+                temp->next=current->next;
+            delete current;
+        }}
 
         void pop_back() {
-            // TODO
-        }
+            if(this->nodes!=0){
+                auto current=this->head;
+                auto temp=new Node<T>;
+                while (current->next!=this->head)
+                    current=current->next;
+                current->next=this->head->next;
+                this->head=this->head->next;
+                delete temp; }}
 
         T operator[](int index) {
-            // TODO
+            auto temp=new Node<T>;
+            if(index>this->nodes)
+                throw length_error("Error");
+            else{
+                for(int i=0;i<index;i++){
+                    temp=temp->next;
+                }
+                return temp->data;
+            }
         }
 
-        bool empty() {
-            // TODO
-        }
+    bool empty() {
+        return this->nodes==0;
+    }
 
-        int size() {
-            // TODO
-        }
+    int size() {
+        return this->nodes;
+    }
 
-        void clear() {
-            // TODO
-        }
+    void clear() {
+        this->head->killSelf();
+        this->head = nullptr;
+        this->tail = nullptr;
+        this->nodes = 0;
+    }
 
         void sort() {
-            // TODO
+            T arr[this->nodes];
+            auto tempo=this->head;
+            for(int i=0; i<this->nodes; ++i){
+                arr[i]=tempo->data;
+                tempo=tempo->next;
+            }
+            for (int i = this->nodes/2; i >0 ; i /=2) {
+                for (int j = i; j <this->nodes; j++) {
+                    int temp=arr[j];
+                    int k;
+                    for ( k = j; k >=i and arr[k-i]>temp; k -=i) {
+                        arr[k] = arr[k-i];
+                    }
+                    arr[k]=temp;
+                }
+            }
+            tempo=this->head;
+            for(int i=0; i<this->nodes; ++i){
+                tempo->data=arr[i];
+                tempo=tempo->next;
+            }
         }
     
         void reverse() {
-            // TODO
+            T arr[this->nodes];
+            auto temp=this->head;
+            for(int i=0; i<this->nodes; ++i){
+                arr[i]=temp->data;
+                temp=temp->next;
+
+            }
+            for(int i=0; i<this->nodes; ++i){
+                this->push_back(arr[i]);
+
+            }
         }
 
         string name() {
@@ -61,15 +147,18 @@ class CircularLinkedList : public List<T> {
         }
 
         BidirectionalIterator<T> begin() {
-            // TODO
+            return BidirectionalIterator<T>(this->head);
         }
 
 	    BidirectionalIterator<T> end() {
-            // TODO
+            return BidirectionalIterator<T>(this->tail);
         }
 
         void merge(CircularLinkedList<T> list) {
-            // TODO
+            for(int i=0;i<list.nodes;i++){
+                this->push_back(list[i]);
+            }
+
         }
 };
 
