f76ce0b1fa067bd997d9594afbe8da3de753d8a6
Francesco Uccelli <39736679+Flrotm@users.noreply.github.com>
2019-09-12 23:51:22 -0500

    Update forward.h

--- forward.h
+++ forward.h
@@ -3,58 +3,132 @@
 
 #include "list.h"
 #include "iterators/forward_iterator.h"
+#include <iostream>
 
+using namespace std;
 template <typename T>
 class ForwardList : public List<T> {
     public:
         ForwardList() : List<T>() {}
 
         T front() {
-            // TODO
+            if(this->head!= nullptr)
+                return this->head->data;
+            else throw invalid_argument("error");
         }
 
         T back() {
-            // TODO
+            if(this->head!= nullptr)
+                return this->tail->data;
+            else throw invalid_argument("error");
         }
 
         void push_front(T value) {
-            // TODO
+            auto temp= new Node<T>;
+            temp->data=value;
+            temp->next=this->head;
+            this->head=temp;
         }
 
         void push_back(T value) {
-            // TODO
+
+            Node<T>* node = new Node<T>;
+            node->data = value;
+            if (this->nodes == 0){
+                this->head = node;
+                this->tail = this->head;
+                this->nodes++;
+            } else {
+                this->tail->next = node;
+                this->tail = node;
+                this->nodes++;
+            }
         }
 
         void pop_front() {
-            // TODO
+            if(this->nodes!=0){
+            this->head=this->head->next;
+            delete this->head->prev;
+            this->nodes--;}
         }
 
         void pop_back() {
-            // TODO
+            auto temp = this->tail;
+            auto node = this->head;
+            while(node->next!=this->tail){
+                node = node->next;
+            }
+            delete temp;
+            this->tail = node;
+            this->nodes--;
         }
 
         T operator[](int index) {
-            // TODO
+
+            if(index>=this->nodes){
+                throw length_error("error");
+            }else {
+                auto *temp = this->head;
+                for (int i=0; i < index; i++) {
+                    temp=temp->next;
+                }
+                return temp->data;
+            }
+
         }
 
         bool empty() {
-            // TODO
+            return this->nodes==0;
         }
 
         int size() {
-            // TODO
+            return this->nodes;
         }
 
         void clear() {
-            // TODO
+            this->head->killSelf();
+            this->head = nullptr;
+            this->tail = nullptr;
+            this->nodes = 0;
         }
 
         void sort() {
-            // TODO
+            T arr[this->nodes];
+            auto tempo=this->head;
+            for(int i=0; i<this->nodes; ++i){
+                arr[i]=tempo->data;
+                tempo=tempo->next;
+            }
+            for (int i = this->nodes/2; i >0 ; i /=2) {
+                for (int j = i; j <this->nodes; j++) {
+                    int temp=arr[j];
+                    int k;
+                    for ( k = j; k >=i and arr[k-i]>temp; k -=i) {
+                        arr[k] = arr[k-i];
+                    }
+                    arr[k]=temp;
+                }
+            }
+            tempo=this->head;
+            for(int i=0; i<this->nodes; ++i){
+                tempo->data=arr[i];
+                tempo=tempo->next;
+            }
         }
     
         void reverse() {
-            // TODO
+            T arr[this->nodes];
+            auto temp=this->head;
+            for(int i=0; i<this->nodes; ++i){
+                arr[i]=temp->data;
+                temp=temp->next;
+
+            }
+            for(int i=0; i<this->nodes; ++i){
+               this->push_back(arr[i]);
+
+            }
+
         }
 
         string name() {
@@ -62,15 +136,17 @@ class ForwardList : public List<T> {
         }
 
         ForwardIterator<T> begin() {
-            // TODO
-        }
+        return ForwardIterator<T>(this->head);
+         }
 
-	    ForwardIterator<T> end() {
-            // TODO
-        }
+        ForwardIterator<T> end() {
+        return ForwardIterator<T>(this->tail->next);
+         }
 
         void merge(ForwardList<T> list) {
-            // TODO
+            for(int i=0;i<list.nodes;i++){
+                this->push_back(list[i]);
+            }
         }
 };
 
